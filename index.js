d9d3ec89da61d0f2e39dab17a8f1581f62d01061
Leon Stam <leon@elephantcs.nl>
2022-07-02 20:41:01 +0200

    Ghetto debounce when getting multiple updates

--- index.js
+++ index.js
@@ -13,10 +13,19 @@ if (!process.env.PUSHBULLET_TOKEN || !process.env.PUSHBULLET_DEVICE_NAME) {
 const deviceId = await getDeviceIdentifier();
 const bot = new Bot(deviceId);
 
+let alreadyFetching = false;
+
 // Setup the websocket for listening to changes
 connect(async () => {
     console.log('On new push, fetching!');
+    if (alreadyFetching) {
+        console.log('=> NVM, already fetching..');
+        return;
+    }
+
+    alreadyFetching = true;
     const push = await getLatestPushFor(deviceId)
+    alreadyFetching = false;
     if (push === undefined) {
         console.log('=> Not for us.');
         return;
